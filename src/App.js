01efe16c55f6b55945a3593e391bd95683e16c60
radiosignalz <93599478+radiosignalz@users.noreply.github.com>
2021-11-05 13:07:06 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,80 @@
+// import { nanoid } from 'nanoid'
+// import {Link,BrowserRouter} from 'react-router-dom'
+import './App.css';
+// import {Layout} from './components'
+// import {Switch,Route} from "react-router-dom";
+// import {Home, Profile, Posts}from "./routes"
+// import {Post} from './routes/Posts/routes'
+import HW4 from "./HW4";
+
+function App() {
+  return (
+    <div className="App">
+
+
+        <HW4/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        {/*<Layout>*/}
+        {/*    <Switch>*/}
+        {/*        <Route path="/posts/:postId" >*/}
+        {/*            <Post/>*/}
+        {/*        </Route>*/}
+        {/*        <Route path="/Profile" >*/}
+        {/*            <Profile/>*/}
+        {/*        </Route>*/}
+        {/*        <Route path="/Posts" >*/}
+        {/*            <Posts/>*/}
+        {/*        </Route>*/}
+        {/*        <Route path="/" >*/}
+        {/*            <Home/>*/}
+        {/*        </Route>*/}
+
+
+        {/*    </Switch>*/}
+
+        {/*</Layout>*/}
+
+
+
+
+        {/*<li key={nanoid}>li</li>*/}
+    </div>
+  );
+}
+
+export default App;
